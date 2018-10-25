10093d89c634ab67e5e9ce110c025defee25b503
Koivunen Sara <a1300842@myy.haaga-helia.fi>
2018-10-25 10:54:19 +0300

    projektin pohja

---
+++ src/main/java/fi/hh/swd4t/zone/fi/hh/swd4t/zone/Application.java
@@ -0,0 +1,12 @@
+package fi.hh.swd4t.zone.fi.hh.swd4t.zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
