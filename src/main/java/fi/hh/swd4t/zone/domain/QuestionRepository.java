5dc93059352f288a7f1bfd8af5251914d6dfd51f
Ari Aaltonen <aaltonengithub@gmail.com>
2018-10-29 09:38:23 +0200

    removed template, question, questionRepo

--- src/main/java/fi/hh/swd4t/zone/domain/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.hh.swd4t.zone.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-public interface QuestionRepository extends CrudRepository<Question, Long> {
-	@Transactional
-	Question findById(int questionId);
-}
--- src/main/java/fi/hh/swd4t/zone/web/ApiController.java
+++ src/main/java/fi/hh/swd4t/zone/web/ApiController.java
@@ -1,16 +1,34 @@
 package fi.hh.swd4t.zone.web;
 
+/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;*/
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-
+	/*@Autowired
+	private QuestionRepository qRepo;
+	*/
 	//Hello world
 	@RequestMapping (value="/api/hello", method=RequestMethod.GET)
 	public String helloWorld() {
 		return "Hello world";
 	}
 	
+	/*Add a question POST
+		@RequestMapping(value = "/addquestion", method = RequestMethod.POST)
+		public String save(Question question, Model model) {
+			qRepo.save(question);
+			model.addAttribute("question", new Question());
+			return "addquestion";
+		}
+	//add a question GET
+		@RequestMapping(value = "/addquestion", method = RequestMethod.GET)
+		public String addquestion(Model model) {
+			model.addAttribute("question", new Question());
+			return "addquestion";
+		}
+		*/
 }
