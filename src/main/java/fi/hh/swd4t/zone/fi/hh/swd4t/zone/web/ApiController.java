71f9d8ce4ca8f2ef6cb8e0f152b746649cd643b0
SHeija <sirkku.heija@myy.haaga-helia.fi>
2018-10-29 08:37:11 +0200

    added hello world

--- src/main/java/fi/hh/swd4t/zone/fi/hh/swd4t/zone/web/ApiController.java
+++ src/main/java/fi/hh/swd4t/zone/fi/hh/swd4t/zone/web/ApiController.java
@@ -1,8 +1,16 @@
 package fi.hh.swd4t.zone.fi.hh.swd4t.zone.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+	//Hello world
+	@RequestMapping (value="/api/hello", method=RequestMethod.GET)
+	public String helloWorld() {
+		return "Hello world";
+	}
+	
 }
