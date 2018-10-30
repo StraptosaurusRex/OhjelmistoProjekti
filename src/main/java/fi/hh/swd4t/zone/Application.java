f871e8815fbe6a3c568a2d8957caa553d5c9e7e3
Sara Koivunen <you@example.com>
2018-10-30 11:50:38 +0200

    linerunner demo

--- src/main/java/fi/hh/swd4t/zone/Application.java
+++ src/main/java/fi/hh/swd4t/zone/Application.java
@@ -1,12 +1,29 @@
 package fi.hh.swd4t.zone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.hh.swd4t.zone.domain.Questionnaire;
+import fi.hh.swd4t.zone.domain.QuestionnaireRepository;
 
 @SpringBootApplication
 public class Application {
+	
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner zoneDemo(QuestionnaireRepository questionrepository){
+		return (args) -> {
+			log.info("tallenna kysymys");
+			questionrepository.save(new Questionnaire("Miten kehittäisit Zonen palveluita?", "null"));
+		};
 	}
 }
